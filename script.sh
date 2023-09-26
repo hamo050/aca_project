61849b97577bbe8fc82b0ab2a90eebed36dc4515
Ubuntu <dzh7438@yandex.ru>
2023-09-26 16:29:58 +0400

    working version

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "RUN Terrafor"
 cd aca_project/terraform
-
 terraform output | grep elastic_ip | awk -F'"' '/"/ {print $2}' >> ../ansible/inventory
 
 mysql_host=$(terraform output | grep db_ip | awk -F'"' '/"/ {print $2}')
